e435d055d4aa598bd519878618d815cc7d01ec65
unknown <clairepatt19@gmail.com>
2021-04-08 20:36:05 -0500

    upda
    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Satellite } from './satellite';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'orbit-report';
+  title = 'Orbit Report';
+  sourceList: Satellite[];
+  // displayList: Satellite[];
+  
+  constructor() {
+    this.sourceList = [];
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+ 
+    window.fetch(satellitesUrl).then(function(response) {
+       response.json().then(function(data) {
+ 
+          let fetchedSatellites = data.satellites;
+            for(let i=0;i<fetchedSatellites.length;i++){
+              this.sourceList.push(new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type,fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType,
+                 fetchedSatellites[i].operational)); 
+               }
+          // make a copy of the sourceList to be shown to the user
+          //  this.displayList = this.sourceList.slice(0);
+       }.bind(this));
+    }.bind(this));
+  }
 }
