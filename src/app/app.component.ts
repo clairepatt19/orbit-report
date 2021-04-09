86270c24547f1900074ad5cdacb72804a6f0a6aa
unknown <clairepatt19@gmail.com>
2021-04-08 23:02:44 -0500

    updated A g

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,12 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'Orbit Report';
   sourceList: Satellite[];
-  // displayList: Satellite[];
+  displayList: Satellite[];
   
   constructor() {
     this.sourceList = [];
+    this.displayList = [];
+    
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
  
     window.fetch(satellitesUrl).then(function(response) {
@@ -23,9 +25,24 @@ export class AppComponent {
               this.sourceList.push(new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type,fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType,
                  fetchedSatellites[i].operational)); 
                }
-          // make a copy of the sourceList to be shown to the user
-          //  this.displayList = this.sourceList.slice(0);
+      
+               this.displayList = this.sourceList.slice(0);        
        }.bind(this));
     }.bind(this));
+    
   }
+  search(searchTerm: string): void {
+    let matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLowerCase();
+    for(let i=0; i < this.sourceList.length; i++) {
+       let name = this.sourceList[i].name.toLowerCase();
+       if (name.indexOf(searchTerm) >= 0) {
+          matchingSatellites.push(this.sourceList[i]);
+       }
+    }
+    // assign this.displayList to be the array of matching satellites
+    // this will cause Angular to re-make the table, but now only containing matches
+    this.displayList = matchingSatellites;
+    console.log(this.displayList);
+ }
 }
--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -15,7 +15,7 @@ export class Satellite
     }    
 
     isSpaceDebris(): boolean {
-        return this.type === 'Space Debris';
+        return this.type.toLowerCase() === 'space debris';
     }
     
 }
